ffb5cb01894fca9f88e697b02268b12642500fc4
mohamedsaeid1998 <105775732+mohamedsaeid1998@users.noreply.github.com>
2024-07-03 01:58:45 +0200

    fix bugs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,26 @@
 import { PrismaClient } from '@prisma/client'
-import { faker } from '@faker-js/faker'
+// import { faker } from '@faker-js/faker'
 
 const prisma = new PrismaClient()
 
 async function main() {
 
-  await prisma.todo.createMany({
-    data: Array.from({ length: 20 }, () => {
-      return {
-        title: faker.lorem.words({ min: 2, max: 5 }),
-        body: faker.lorem.words({ min: 1, max: 10 }),
-      }
-    })
-  })
+  // await prisma.todo.createMany({
+  //   data: Array.from({ length: 20 }, () => {
+  //     return {
+  //       title: faker.lorem.words({ min: 2, max: 5 }),
+  //       body: faker.lorem.words({ min: 1, max: 10 }),
+  //       user_id:"user_2hqRielRkVneOqQ7qBp3mXLug18"
+  //     }
+  //   })
+  // })
 }
 
-main()
-  .catch(async (e) => {
-    console.error(e)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+// main()
+//   .catch(async (e) => {
+//     console.error(e)
+//     process.exit(1)
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect()
+//   })
